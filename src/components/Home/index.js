d6af83178f7c11dec69e16c8edfb0cd1bb55665d
Teddy Horvath <teddyhorvath@gmail.com>
2022-10-19 16:03:19 -0700

    fixing hover effect again

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,13 +8,13 @@ import Loader from 'react-loaders'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  const nameArray = ['e','d','d','y']
-  const jobArray = ['c','o','m','p','u','t','e','r',' ','s','c','i','e','n','c','e',' ','u','n','d','e','r','g','r','a','d']
+  const nameArray = ['e','d','d','y',' ', 'a',',']
+  const jobArray = ['C','o','m','p','u','t','e','r',' ','S','c','i','e','n','c','e',' ','u','n','d','e','r','g','r','a','d','u','a','t','e','.']
 
   useEffect(() => {
     setTimeout(() => {
       setLetterClass('text-animate-hover')
-    }, 4000)
+    }, 7000)
   }, [])
 
   return (
@@ -30,7 +30,7 @@ const Home = () => {
           <img src={LogoT} alt='dev' />
           <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
           <br />
-          <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22} />
+          <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={20} />
           </h1>
           <h2>Fullstack Developer</h2>
           <Link to='/contact' className='flat-button'>CONTACT ME</Link>
