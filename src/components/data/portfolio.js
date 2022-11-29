bcb18bed22cfa54988e12429fa343b0d4603180d
Teddy Horvath <teddyhorvath@gmail.com>
2022-11-28 17:01:59 -0800

    adding pong to portfolio

--- src/components/data/portfolio.js
+++ src/components/data/portfolio.js
@@ -1,27 +1,51 @@
 
     export const portfolioData = [
         {
-            "cover": require("../../portfolio/1/sync.JPG"),
+            "cover": require("../../portfolio/2/svm.JPG"),
             "title": "Scotts Valley Magnetics Website",
             "description": "Company website developed for electronics manufacturing company Scotts Valley Magnetics.",
             "stack": "ReactJS, JSX",
-            "url": "https://github.com/HorvathTeddy/svmagnetics",
-            "src": "https://scottsvalleymagnetics.netlify.app/"
+            "url": "https://scottsvalleymagnetics.netlify.app/",
+            "src": "https://github.com/HorvathTeddy/svmagnetics"
         },
         {
-            "cover": require("../../portfolio/2/svm.JPG"),
+            "cover": require("../../portfolio/1/sync.JPG"),
             "title": "Sync Webapp",
             "description": "Music collaboration app utilizing advanced AI and machine learning to match artists, lyricists, and producers.",
             "stack": "ReactJS, Firebase",
-            "url": "https://github.com/HorvathTeddy/Sync",
-            "src": "https://github.com/HorvathTeddy/Sync"
+            "url": "https://scottsvalleymagnetics.netlify.app/",
+            "src": "https://github.com/HorvathTeddy/sync"
         },
         {
             "cover": require("../../portfolio/3/uhr.PNG"),
             "title": "Uhr Mobile Delivery",
-            "description": "On demand cannabis delivery service with a user and driver application.",
+            "description": "On demand delivery service with a user and driver application.",
             "stack": "React Native, AWS Amplify",
             "url": "https://github.com/HorvathTeddy/SprintDeliveries",
             "src": "https://github.com/HorvathTeddy/SprintDeliveries"
+        },
+        {
+            "cover": require("../../portfolio/4/tinder.JPG"),
+            "title": "Tinder Clone",
+            "description": "Full stack tinder clone.",
+            "stack": "ReactJS, MongoDB, Express, NodeJS",
+            "url": "https://tinder-clone-e5610.firebaseapp.com/",
+            "src": "https://github.com/HorvathTeddy/tinder-clone"
+        },
+        {
+            "cover": require("../../portfolio/5/taskmaster.JPG"),
+            "title": "Task-R-Us",
+            "description": "A full stack task manager application for business owners",
+            "stack": "React Native, MongoDB, Express, NodeJS",
+            "url": "https://github.com/HorvathTeddy/taskmaster",
+            "src": "https://github.com/HorvathTeddy/taskmaster"
+        },
+        {
+            "cover": require("../../portfolio/6/pongjs.JPG"),
+            "title": "Pong",
+            "description": "Pong in web browser with AI opponent",
+            "stack": "JavaScript",
+            "url": "https://pong-in-js.netlify.app/",
+            "src": "https://github.com/HorvathTeddy/pong-js"
         }
     ]
